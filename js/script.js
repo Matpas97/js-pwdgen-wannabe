1a5d512f574614cac9cb9655b17898b2a6c76dbe
Matpas97 <passarellim22@gmail.com>
2022-05-31 17:01:29 +0200

    Final Message

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/// Chiedi all'utente il suo nome, cognome,il suo colore preferito 
+/// Inserire in pagina nomecognomecolorepreferito21
+
+/// Nome Utente
+
+let userName = prompt( 'Inserisci il tuo nome ');
+
+/// Cognome Utente 
+
+let userSurname = prompt ( 'Inserisci il tuo Cognome ');
+
+/// Colore Preferito
+
+let userColor = prompt ( 'Dimmi il tuo colore preferito')
+
+/// Creo il messaggio finale per l'utente
+let userMessage = userName + userSurname + userColor;
